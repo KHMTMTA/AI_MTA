701138187d6088f52fd745d61122c631c6bdff68
Nguyen Huu Hoan <nguyenhuuhoan97@gmail.com>
2018-06-14 00:47:45 +0700

    [Hoàn] Form Tic Tac Toe - AI

---
+++ Tic Tac Toe - AI/Tic Tac Toe - AI/Form1.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace Tic_Tac_Toe___AI
+{
+    partial class frmForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmForm));
+            this.SuspendLayout();
+            // 
+            // frmForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(649, 432);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmForm";
+            this.Text = "Tic Tac Toe - Artificical Intelligence";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
